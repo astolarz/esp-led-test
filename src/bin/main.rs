40bcf0eed18ef29d517040a59a6e2db44e58bc74
Aaron Stolarz <aaronstolarz@gmail.com>
2025-02-01 22:23:20 -0800

    Add matrix animation code.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,9 +4,9 @@
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
 use esp_hal::delay::Delay;
-use esp_hal::peripherals::Peripherals;
+use esp_hal::peripherals::RNG;
 use esp_hal::rng::Rng;
-use esp_hal::main;
+use esp_hal::{main, Blocking};
 use esp_hal::spi::Mode;
 use esp_hal::spi::master::{Config, Spi};
 use smart_leds::{SmartLedsWrite, RGB8};
@@ -19,6 +19,7 @@ extern crate alloc;
 const NUM_LEDS: usize = 144;
 
 const RED: RGB8 = RGB8::new(40, 0, 0);
+#[allow(dead_code)]
 const ORANGE: RGB8 = RGB8::new(40, 20, 0);
 const YELLOW: RGB8 = RGB8::new(40, 40, 0);
 const GREEN: RGB8 = RGB8::new(0, 40, 0);
@@ -37,24 +38,29 @@ fn main() -> ! {
 
     esp_alloc::heap_allocator!(72 * 1024);
 
-    cat_toy(peripherals);
-    // rainbow(peripherals);
-}
-
-fn cat_toy(peripherals: Peripherals) -> ! {
     let pin15 = esp_hal::gpio::Output::new(peripherals.GPIO15, esp_hal::gpio::Level::High);
     let spi = Spi::new(peripherals.SPI2, Config::default().with_frequency(HertzU32::MHz(3)).with_mode(Mode::_0))
         .unwrap().with_mosi(pin15);
     let mut ws = Ws2812::new(spi);
+    let leds = [RGB8::default(); 144];
+    ws.write(leds).unwrap();
+
+    cat_toy(ws, peripherals.RNG);
+    // rainbow(peripherals);
+    // matrix(ws, peripherals.RNG);
+}
+
+#[allow(dead_code)]
+fn cat_toy(mut ws: Ws2812<Spi<'_, Blocking>>, rng: RNG) -> ! {
     let mut leds = [RGB8::default(); NUM_LEDS];
     let colors = [RED, PURPLE, BLUE, CYAN, GREEN, YELLOW, ORANGE];
-    let mut rng = Rng::new(peripherals.RNG);
+    let mut rng = Rng::new(rng);
     
     let mut pos = rng.random() % NUM_LEDS as u32;
     let mut direction: i32 = if rng.random() % 2 == 0 { 1 } else { -1 };
     let mut color_iter = colors.into_iter().cycle();
     let mut current_color = color_iter.next().unwrap();
-    let use_colors = false;
+    let use_colors = true;
 
     loop {
         let distance = rng.random() % (
@@ -64,6 +70,7 @@ fn cat_toy(peripherals: Peripherals) -> ! {
                 pos
             }
         );
+        info!("{pos}");
         for _ in 0..distance {
             if use_colors {
                 leds[pos as usize] = current_color;
@@ -81,11 +88,7 @@ fn cat_toy(peripherals: Peripherals) -> ! {
 }
 
 #[allow(dead_code)]
-fn rainbow(peripherals: Peripherals) -> ! {
-    let pin15 = esp_hal::gpio::Output::new(peripherals.GPIO15, esp_hal::gpio::Level::High);
-    let spi = Spi::new(peripherals.SPI2, Config::default().with_frequency(HertzU32::MHz(3)).with_mode(Mode::_0))
-        .unwrap().with_mosi(pin15);
-    let mut ws = Ws2812::new(spi);
+fn rainbow(mut ws: Ws2812<Spi<'_, Blocking>>) -> ! {
     let mut leds = [RGB8::default(); NUM_LEDS];
     let colors = [RED, GREEN, BLUE];
     let colors2 = [PURPLE, YELLOW, CYAN];
@@ -118,5 +121,52 @@ fn rainbow(peripherals: Peripherals) -> ! {
         } else {
             led_idx2 + 1
         };
+    }
+}
+
+#[allow(dead_code)]
+fn matrix(mut ws: Ws2812<Spi<'_, Blocking>>, _rng: RNG) -> ! {
+    const LEAD_LED: RGB8 = RGB8::new(10, 160, 10);
+    const SUB: u8 = 1;
+    let mut leds = [RGB8::default(); NUM_LEDS];
+    let mut head: usize = 0;
+    let mut tail: usize = 0;
+    
+    leds[head] = LEAD_LED;
+    ws.write(leds).unwrap();
+
+    loop {
+        for i in tail..head {
+            let rgb = leds[i].clone();
+            let new_rgb = RGB8::new(rgb.r.saturating_sub(SUB), rgb.g.saturating_sub(SUB), rgb.b.saturating_sub(SUB));
+            if rgb.g != 0 && new_rgb.g == 0 {
+                tail = i;
+            }
+            leds[i] = new_rgb;
+            // info!("i: {i}, head: {head}, tail: {tail}");
+        }
+
+        if head == NUM_LEDS - 1 {
+            // info!("edge case head: {head}, tail: {tail}");
+            let rgb = leds[head].clone();
+            let new_rgb = RGB8::new(rgb.r.saturating_sub(SUB), rgb.g.saturating_sub(SUB), rgb.b.saturating_sub(SUB));
+            leds[head] = new_rgb;
+        }
+
+        ws.write(leds).unwrap();
+        if head >= (NUM_LEDS - 1) {
+            if head == tail {
+                tail = 0;
+                head = 0;
+            } else {
+                tail = tail + 1;
+            }
+        } else {
+            head = head + 1;
+            leds[head] = LEAD_LED;
+            // info!("new head");
+        };
+        
+        // info!("head: {head}, tail: {tail}");
     }
 }
